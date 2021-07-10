68961156906b76ce17cd1e7d421c2a25002cdcce
modsoul42454 <34451508+modsoul42454@users.noreply.github.com>
2021-07-09 19:43:32 -0500

    Seems to work with ppinch to zoom and draggin

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ window.onload = function () {
             autoCenter: Phaser.Scale.CENTER_BOTH,
             parent: "thegame",
             width: 650,
-            height: 650 
+            height: 650
         },
         physics: {
             default: "arcade",
@@ -82,20 +82,43 @@ class playGame extends Phaser.Scene {
         super("PlayGame");
     }
     preload() {
+        var url;
 
+        url = 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexpinchplugin.min.js';
+        this.load.plugin('rexpinchplugin', url, true);
     }
 
     create() {
+        this.dragScale = this.plugins.get('rexpinchplugin').add(this);
+        var camera = this.cameras.main;
+        this.pinch_zoom_flag = false
+        this.dragScale
+            .on('pinch', function (dragScale) {
+                if (!this.dragObj === null) {
+                    this.dragObj.x = this.dragObj.last_pos_x
+                    this.dragObj.y = this.dragObj.last_pos_y
+                    this.dragObj = null
+                }
+                var scaleFactor = dragScale.scaleFactor;
+                camera.zoom *= scaleFactor;
+                console.log(camera.scrollX)
+                var drag1Vector = dragScale.drag1Vector;
+                camera.scrollX -= drag1Vector.x / camera.zoom;
+                camera.scrollY -= drag1Vector.y / camera.zoom;
+            }, this)
         array_rects = createArray(num_x, num_y)
         array_groups = createArray(num_x, num_y)
         var array_ii = Array()
         var array_jj = Array()
         var array_count = Array()
-        this.score_text = this.add.text(10 , num_y*spacer + init_y,'test', { color: rgbToHex(0,0,0) })
+        this.score_text = this.add.text(10, num_y * spacer + init_y, 'test', { color: rgbToHex(0, 0, 0) })
         var count = 0
+
+        this.input.dragTimeThreshold = 0.
+        this.input.addPointer(4)
         for (var ii = 0; ii < num_x; ii++) {
             for (var jj = 0; jj < num_x; jj++) {
-                
+
                 var r = ii / num_x * 200 + 55
                 var g = (ii + jj) / (sq_size * 2)
                 var b = jj / num_y * 255
@@ -110,14 +133,15 @@ class playGame extends Phaser.Scene {
                 rect1.last_pos_x = rect1.x
                 rect1.last_pos_y = rect1.y
                 array_rects[ii][jj] = rect1
+
                 array_rects[ii][jj].orig_pos_x = array_rects[ii][jj].x
                 array_rects[ii][jj].orig_pos_y = array_rects[ii][jj].y
 
 
                 // array_rects[ii][jj].visible = false
-                array_rects[ii][jj].disableInteractive()
-                
-                array_rects[ii][jj].setInteractive()
+                // array_rects[ii][jj].disableInteractive()
+
+                array_rects[ii][jj].setInteractive({ draggable: true })
                 // array_rects[ii][jj].add.text('dsd')
                 array_ii.push(ii)
                 array_jj.push(jj)
@@ -126,10 +150,11 @@ class playGame extends Phaser.Scene {
 
             }
         }
-        
-        this.input.on('pointerdown', this.startdrag, this)
-        reload_data = true
-        if (reload_data){
+
+        this.input.on('pointerdown', this.PointerDown, this)
+        // this.input.on('pointer2down', this.pointer2_down, this)
+        reload_data = false
+        if (reload_data) {
             var recover_array = JSON.parse(localStorage.getItem('Array'))
             var recover_text_marking_array = JSON.parse(localStorage.getItem('text_array'))
             var array_text_jj = JSON.parse(localStorage.getItem('array_text_jj'))
@@ -138,19 +163,19 @@ class playGame extends Phaser.Scene {
                 for (var jj = 0; jj < num_x; jj++) {
                     array_rects[ii][jj].x = recover_array[ii][jj].x
                     array_rects[ii][jj].y = recover_array[ii][jj].y
-                    array_rects[ii][jj].last_pos_x =  array_rects[ii][jj].x
-                    array_rects[ii][jj].last_pos_y =  array_rects[ii][jj].y
+                    array_rects[ii][jj].last_pos_x = array_rects[ii][jj].x
+                    array_rects[ii][jj].last_pos_y = array_rects[ii][jj].y
                     array_rects[ii][jj].setInteractive()
                     array_rects[ii][jj].flag_interactive = true
 
-                    
-                    
+
+
                 }
             }
-            
-            for (var itx = 0; itx <recover_text_marking_array.length; itx++){
+
+            for (var itx = 0; itx < recover_text_marking_array.length; itx++) {
                 var text_mark = recover_text_marking_array[itx]
-                var text = this.add.text(text_mark.x, text_mark.y,'o', { color: rgbToHex(0,0,0) })
+                var text = this.add.text(text_mark.x, text_mark.y, 'o', { color: rgbToHex(0, 0, 0) })
                 text.depth = text_mark.depth
                 array_rects[array_text_ii[itx]][array_text_jj[itx]].disableInteractive()
                 array_rects[array_text_ii[itx]][array_text_jj[itx]].flag_interactive = false
@@ -159,84 +184,145 @@ class playGame extends Phaser.Scene {
 
         }
 
-        if (!reload_data)
-        {var count = 0
-        this.array_text =  []
-        var array_text_ii = []
-        var array_text_jj = []
-        for (var ii = 0; ii < num_x; ii++)
-            for (var jj = 0; jj < num_y; jj++) {
-                {
-                    array_rects[ii][jj].flag_interactive = true
-                    //  |
-                    if ( Math.random() < .9 ) {
-                        var graphics = this.add.graphics()
-                        array_rects[ii][jj].x = init_x + Math.random() * num_x * (spacer)
-                        array_rects[ii][jj].y = init_y + Math.random() * num_y * (spacer)
-                        this.find_and_swap(array_rects[ii][jj]);
-                        array_rects[ii][jj].last_pos_x = array_rects[ii][jj].x
-                        array_rects[ii][jj].last_pos_y = array_rects[ii][jj].y
-                        // array_rects[ii][jj].visible = false
+        if (!reload_data) {
+            var count = 0
+            this.array_text = []
+            var array_text_ii = []
+            var array_text_jj = []
+            for (var ii = 0; ii < num_x; ii++)
+                for (var jj = 0; jj < num_y; jj++) {
+                    {
                         array_rects[ii][jj].flag_interactive = true
-                        array_rects[ii][jj].setInteractive();
-                        // array_rects[ii][jj].color = rgbToHex(0,0,0)
-                        // this.add.text(array_rects[ii][jj].x , array_rects[ii][jj].y  ,
-                        //      'o', { color: rgbToHex(0,0,0) }).setOrigin(0, 0);
-                        
-                    }
-                    else{
-                        
-                        var text = this.add.text(array_rects[ii][jj].x - spacer/4, array_rects[ii][jj].y -spacer/4,'o', { color: rgbToHex(0,0,0) })
-                        text.depth = 1000
-                        array_text_ii.push(ii)
-                        array_text_jj.push(jj)
-                        array_rects[ii][jj].disableInteractive();
-                        array_rects[ii][jj].flag_interactive = false
-                        this.array_text.push(text)
+                        //  |
+                        if (Math.random() < .9) {
+                            var graphics = this.add.graphics()
+                            array_rects[ii][jj].x = init_x + Math.random() * num_x * (spacer)
+                            array_rects[ii][jj].y = init_y + Math.random() * num_y * (spacer)
+                            this.find_and_swap(array_rects[ii][jj]);
+                            array_rects[ii][jj].last_pos_x = array_rects[ii][jj].x
+                            array_rects[ii][jj].last_pos_y = array_rects[ii][jj].y
+                            // array_rects[ii][jj].visible = false
+                            array_rects[ii][jj].flag_interactive = true
+                            array_rects[ii][jj].setInteractive();
+                            // array_rects[ii][jj].color = rgbToHex(0,0,0)
+                            // this.add.text(array_rects[ii][jj].x , array_rects[ii][jj].y  ,
+                            //      'o', { color: rgbToHex(0,0,0) }).setOrigin(0, 0);
+
+                        }
+                        else {
+
+                            var text = this.add.text(array_rects[ii][jj].x - spacer / 4, array_rects[ii][jj].y - spacer / 4, 'o', { color: rgbToHex(0, 0, 0) })
+                            text.depth = 1000
+                            array_text_ii.push(ii)
+                            array_text_jj.push(jj)
+                            array_rects[ii][jj].disableInteractive();
+                            array_rects[ii][jj].flag_interactive = false
+                            this.array_text.push(text)
+                        }
                     }
                 }
-            }
-        console.log(count)
-        var string_data  = JSON.stringify(this.array_text)
-        localStorage.setItem('text_array',string_data)
-        localStorage.setItem('array_text_ii',JSON.stringify(array_text_ii))
-        localStorage.setItem('array_text_jj',JSON.stringify(array_text_jj))
-       }
-        
-      
-    }
-
-
-    startdrag(pointer, targets) {
-        if (targets[0] != null) {
-
-
-            this.input.off('pointerdown', this.startdrag, this)
-            this.dragObj = targets[0]
-            this.dragObj.depth = 100
-
-
-            this.input.on('pointermove', this.doDrag, this)
-            this.input.on('pointerup', this.stopDrag, this)
+            console.log(count)
+            var string_data = JSON.stringify(this.array_text)
+            localStorage.setItem('text_array', string_data)
+            localStorage.setItem('array_text_ii', JSON.stringify(array_text_ii))
+            localStorage.setItem('array_text_jj', JSON.stringify(array_text_jj))
         }
-    }
 
-    doDrag(pointer) {
-        this.dragObj.x = pointer.x
-        this.dragObj.y = pointer.y
 
     }
 
-    stopDrag(pointer, targets) {
-        this.input.on('pointerdown', this.startdrag, this)
-        this.dragObj.depth = 0
-        this.input.off('pointermove', this.doDrag, this)
-        this.input.off('pointerup', this.stopDrag, this)
 
-
-
-        this.find_and_swap(this.dragObj)
+    pointer2_down() {
+        console.log('Pointer 2 down')
     }
+    PointerDown(pointer, targets) {
+        console.log('isPinched' + this.dragScale.isPinched)
+        if (this.input.pointer1.active && !this.input.pointer2.active && !this.dragScale.isPinched) {
+            if (targets[0] != null) {
+
+
+                // this.input.on('pointerdown', this.PointerDown, this)
+                this.dragObj = targets[0]
+
+                this.rect1 = this.dragObj
+                this.rect1.on('drag', (pointer, dragX, dragY) => {
+                    if (!this.dragScale.isPinched){
+                    console.log('Draggin')
+                    this.rect1.x = dragX
+                    this.rect1.y = dragY
+                    // var drag1Vector = dragScale.drag1Vector;
+                    // camera.scrollX -= drag1Vector.x / camera.zoom;
+                    // camera.scrollY -= drag1Vector.y / camera.zoom;
+                    this.rect1.depth = 100}
+                });
+                this.rect1.on('dragend', (pointer, dragX, dragY) => {
+                    console.log('end draggin')
+                    this.rect1.depth = 1
+                    this.find_and_swap(this.rect1)
+                })
+
+
+
+
+            }
+        }
+
+        // if (this.input.pointer1.active && this.input.pointer2.active) {
+
+        //     console.log("both pointers active")
+        //     this.dragObj = targets[0]
+
+        //     var rect1 = this.dragObj
+        //     rect1.off('drag', (pointer, dragX, dragY) => {
+        //         console.log('Draggin')
+        //         rect1.x = dragX
+        //         rect1.y = dragY
+        //     });
+        //     rect1.off('dragend', (pointer, dragX, dragY) => {
+        //         console.log('end draggin')
+        //         this.find_and_swap(rect1)
+        //     })
+
+        //     this.dragObj.x = this.dragObj.last_pos_x
+        //     this.dragObj.y = this.dragObj.last_pos_y
+        //     // this.input.off('pointermove', this.doDrag, this)
+        //     // this.input.off('pointerup', this.stopDrag, this)
+        // }
+    }
+
+    // doDrag(pointer) {
+    //     var rect1 = this.dragObj
+    //     rect1.on('drag',(pointer,dragX,dragY) => {
+    //         console.log('Draggin')
+    //         rect1.x = dragX
+    //         rect1.y = dragY
+    //     });
+    //     rect1.on('dragend', (pointer, dragX, dragY) =>{
+    //         console.log('end draggin')
+    //         this.find_and_swap(rect1)
+    //     });
+
+
+    //     if (this.input.pointer1.active && this.input.pointer2.active) {
+    //     //     console.log("both pointers active")
+    //     //     this.dragObj.x = this.dragObj.last_pos_x
+    //     //     this.dragObj.y = this.dragObj.last_pos_y
+    //     //     this.input.off('pointermove', this.doDrag, this)
+    //     //     this.input.off('pointerup', this.stopDrag, this)
+    //     }
+
+    // }
+
+    // stopDrag(pointer, targets) {
+    //     this.input.on('pointerdown', this.PointerDown, this)
+    //     this.dragObj.depth = 0
+    //     this.input.off('pointermove', this.doDrag, this)
+    //     this.input.off('pointerup', this.stopDrag, this)
+
+
+
+    //     this.find_and_swap(this.dragObj)
+    // }
 
     find_and_swap(rect_in) {
 
@@ -244,18 +330,18 @@ class playGame extends Phaser.Scene {
         var new_x = Math.round(rect_in.x / spacer) * spacer
         var new_y = Math.round(rect_in.y / spacer) * spacer
         var sub_found = false
-       
+
         for (var ii = 0; ii < num_x; ii++)
             for (var jj = 0; jj < num_x; jj++) {
-                {                   
+                {
                     if (array_rects[ii][jj] != rect_in & equals(array_rects[ii][jj].x, new_x) & equals(array_rects[ii][jj].y, new_y)) {
                         var intersected_array = array_rects[ii][jj]
                         sub_found = true
                     }
                 }
             }
-            
-        
+
+
         if (sub_found && intersected_array.flag_interactive) {
             intersected_array.x = rect_in.last_pos_x
             intersected_array.y = rect_in.last_pos_y
@@ -289,8 +375,7 @@ class playGame extends Phaser.Scene {
                     // var intersection_data = Phaser.Geom.Intersects.GetRectangleIntersection(rect_in, array_rects[ii][jj]);  
                     // var intersection = Phaser.Geom.Intersects.RectangleToRectangle(array_rects[ii][jj], rect_in);    
 
-                    if (array_rects[ii][jj].x == array_rects[ii][jj].orig_pos_x && array_rects[ii][jj].y == array_rects[ii][jj].orig_pos_y )
-                    {
+                    if (array_rects[ii][jj].x == array_rects[ii][jj].orig_pos_x && array_rects[ii][jj].y == array_rects[ii][jj].orig_pos_y) {
                         correct_num++
                     }
                     if (array_rects[ii][jj] != rect_in & equals(array_rects[ii][jj].x, new_x) & equals(array_rects[ii][jj].y, new_y)) {
@@ -299,15 +384,19 @@ class playGame extends Phaser.Scene {
                     }
                 }
             }
-            
-        this.score_text.text =  correct_num +'/' + num_y*num_y
+
+        this.score_text.text = correct_num + '/' + num_y * num_y
         // array_rects )
-        var string_data  = JSON.stringify(array_rects)
-        localStorage.setItem('Array',string_data)
-        
+        var string_data = JSON.stringify(array_rects)
+        localStorage.setItem('Array', string_data)
+
         // recover_array = JSON.parse(string_data)
     }
-    
+
+    update() {
+       
+    }
+
 
 }
 
