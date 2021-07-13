9024854766e281c6e187c32a4cb98b324cbf0a88
modsoul42454 <34451508+modsoul42454@users.noreply.github.com>
2021-07-13 18:10:22 -0500

    save

--- public/game.js
+++ public/game.js
@@ -1,31 +1,3 @@
-// // Firebase App (the core Firebase SDK) is always required and must be listed first
-// import firebase from "firebase/app";
-// // If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
-// // import * as firebase from "firebase/app"
-
-// // If you enabled Analytics in your project, add the Firebase SDK for Analytics
-// // import "firebase/analytics";
-
-// // Add the Firebase products that you want to use
-// import "firebase/auth";
-// import "firebase/firestore";
-
-// // TODO: Replace the following with your app's Firebase project configuration
-// // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBFBGq0E6rygcGDysGx6PvM3pKpB3Sp6oI",
-//     authDomain: "colorgame-3c359.firebaseapp.com",
-//     projectId: "colorgame-3c359",
-//     storageBucket: "colorgame-3c359.appspot.com",
-//     messagingSenderId: "394017745851",
-//     appId: "1:394017745851:web:23bb8f730bfff5baa3729e"
-//   };
-  
-//   // Initialize Firebase
-//   firebase.initializeApp(firebaseConfig);
-
-  
-
 window.onload = function () {
     let gameConfig = {
         type: Phaser.AUTO,
